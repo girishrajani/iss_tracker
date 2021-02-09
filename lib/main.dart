298395ffed1b0e19e9edc4c2707542c060b807eb
Girish Rajani <girish.r.rajani@gmail.com>
2021-02-09 13:03:48 +0530

    UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,78 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 void main() {
-  _getIss();
+  runApp(
+    MaterialApp(
+      home: IssTracker(),
+    ),
+  );
+}
+
+class IssTracker extends StatefulWidget {
+  @override
+  _IssTrackerState createState() => _IssTrackerState();
+}
+
+class _IssTrackerState extends State<IssTracker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('ISS Tracker'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.help_center_rounded,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.blue[900],
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Center(
+                  child: Text('ISS Tracker'),
+                ),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: Icon(
+                  Icons.people,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'People on ISS',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Exit',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 Future<void> _getIss() async {
