b2f762ecb302ceaf0624b5fa1e3972511a2e6498
Girish Rajani <girish.r.rajani@gmail.com>
2021-02-09 12:36:21 +0530

    API Handled

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-void main() {}
+void main() {
+  _getIss();
+}
+
+Future<void> _getIss() async {
+  var url = "http://api.open-notify.org/iss-now.json";
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    var lat = jsonResponse['iss_position']["latitude"];
+    var long = jsonResponse['iss_position']["longitude"];
+    print('Latitude: $lat.');
+    print('Longitude: $long.');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
+}
