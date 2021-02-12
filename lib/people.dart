d3de14c4004b2a36f6ce97e4dcd009b9d7396a92
Girish Rajani <girish.r.rajani@gmail.com>
2021-02-12 22:53:24 +0530

    Links Handled

--- lib/people.dart
+++ lib/people.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(People());
+}
 
 class People extends StatelessWidget {
   @override
@@ -56,6 +61,16 @@ class _GetDataState extends State<GetData> {
     return data1;
   }
 
+  void _openLink(String astroname) async {
+    var url = 'https://duckduckgo.com/?q=';
+    var _mainUrl = url + astroname;
+    if (await canLaunch(_mainUrl)) {
+      await launch(_mainUrl);
+    } else {
+      throw 'Could not launch $_mainUrl';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -80,22 +95,26 @@ class _GetDataState extends State<GetData> {
             itemBuilder: (BuildContext context, int index) {
               return Padding(
                 padding: EdgeInsets.all(10),
-                child: Card(
-                  color: Colors.red,
-                  child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(30),
-                      child: Text(
-                        snapshot.data[index].name,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
+                child: InkWell(
+                  onTap: () {
+                    _openLink(snapshot.data[index].name);
+                  },
+                  splashColor: Colors.pink,
+                  child: Card(
+                    color: Colors.red,
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text(
+                          snapshot.data[index].name,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
                         ),
                       ),
                     ),
                   ),
-
-                  // child: Text('test'),
                 ),
               );
             },
